3f1a249926ab9a6a7e3a2e23a3ed82ca02478265
alextanghk <alextang1030@gmail.com>
2021-10-24 08:49:32 +0100

    update code

--- src/pages/Purchase/ShoppingCart.tsx
+++ src/pages/Purchase/ShoppingCart.tsx
@@ -3,7 +3,7 @@ import { withRouter } from 'react-router';
 import { Helmet  } from "react-helmet-async";
 import { useTranslation } from 'react-i18next';
 import BWPMerchant from '../../components/merchants';
-import { BWPBreadCrumb, BWPCheckbox, BWPImgInput, BWPNumber } from '../../components/common';
+import { BWPBreadCrumb, BWPCheckbox, BWPImgInput, BWPNumber, BWPButton } from '../../components/common';
 import { Link } from 'react-router-dom';
 
 // Demo Only
@@ -41,13 +41,21 @@ const ShoppingCartPage = () => {
                                 <span className="font-family-noto text-bwp-green">HKD$11.00</span>
                             </div>
                         </div>
-                        <div className={`px-4 self-center flex-initial`}><BWPNumber textAlign="center" vertical inputSize="md"/></div>
+                        <div className={`px-4 self-center flex-initial`}><BWPNumber textAlign="center" min={0} vertical inputSize="md"/></div>
                     </div>
                     
                 </div>
             </div>
             <div className={`md:w-1/3`}>
-
+                <div className="bg-bwp-light-green md:bg-bwp-white md:mx-2 grid grid-cols-3 md:grid-cols-2 p-2 gap-2 text-bwp-white md:text-bwp-dark-grey">
+                    <div>共1件</div>
+                    <div className={`md:col-start-1 md:row-start-2`}>合計：$11</div>
+                    <div className={`text-right md:row-span-2`}>
+                        <BWPButton color="green" className="mr-1 shadow">
+                            <span className="text-bwp-white md:text-base text-sm">{t("lb_submit_order")}</span>
+                        </BWPButton>
+                    </div>
+                </div>
             </div>
         </div>
     </>)
